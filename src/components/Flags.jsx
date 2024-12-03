ba32815af6cde5d525182c605d8816ab51dd19bb
Chirag Soni <csoni0693@gmail.com>
2024-12-03 16:12:35 +0530

    portfolio

--- src/components/Flags.jsx
+++ src/components/Flags.jsx
@@ -7,19 +7,19 @@ const Flags = () => {
     <div className="w-full mt-20 flex justify-center items-center gap-10">
          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 w-full max-w-5xl cursor-pointer">
  <div className='flex flex-col items-center justify-center gap-2 p-2 border border-white shadow-2xl shadow-gray-600 rounded-md'>
-          <h1 className='text-[#08D665] font-bold text-3xl'>2023+</h1>
+          <h1 className='text-[#08D665] font-bold text-3xl'>50+</h1>
           <h2 className={` ${darkMode?"text-white":"text-gray-500"} font-medium text-lg`}>Successful Projects</h2>
         </div>
         <div className='flex flex-col items-center justify-center gap-2 p-2 border border-white shadow-2xl shadow-gray-600 rounded-md'>
-          <h1 className='text-[#08D665] font-bold text-3xl'>340K</h1>
+          <h1 className='text-[#08D665] font-bold text-3xl'>3+</h1>
           <h2 className={` ${darkMode?"text-white":"text-gray-500"} font-medium text-lg`}>Happy Clients</h2>
         </div>
         <div className='flex flex-col items-center justify-center gap-2 p-2 border border-white shadow-2xl shadow-gray-600 rounded-md'>
-          <h1 className='text-[#08D665] font-bold text-3xl'>490+</h1>
+          <h1 className='text-[#08D665] font-bold text-3xl'>2+</h1>
           <h2 className={` ${darkMode?"text-white":"text-gray-500"} font-medium text-lg`}>Team Members</h2>
         </div>
         <div className='flex flex-col items-center justify-center gap-2 p-2 border border-white shadow-2xl shadow-gray-600 rounded-md'>
-          <h1 className='text-[#08D665] font-bold text-3xl'>100+</h1>
+          <h1 className='text-[#08D665] font-bold text-3xl'>2+</h1>
           <h2 className={` ${darkMode?"text-white":"text-gray-500"} font-medium text-lg`}>Winning Awards</h2>
         </div>
     </div>
